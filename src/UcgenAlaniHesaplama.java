d0e4aeb11e41b5f9f668d2469aa0a57493a62dfa
Emine Özbek <77543307+EmineOzbek@users.noreply.github.com>
2022-03-11 19:23:22 +0300

    Third homework

---
+++ src/UcgenAlaniHesaplama.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class UcgenAlaniHesaplama {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Kenar uzunlugu girin: ");
+		int kenar1 = input.nextInt();
+		System.out.print("Kenar uzunlugu girin: ");
+		int kenar2 = input.nextInt();
+		System.out.print("Kenar uzunlugu girin: ");
+		int kenar3 = input.nextInt();
+
+		int x = (kenar1 + kenar2 + kenar3) / 2;
+		double a = x * (x - kenar1) * (x - kenar2) * (x - kenar3);
+		double alan = Math.sqrt(a);
+		System.out.println("Ucgenin alani: " + alan);
+
+		input.close();
+
+	}
+
+}
