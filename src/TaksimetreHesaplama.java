ec24314d10c4d1d2cb4c277e289868ff33f56398
Emine Özbek <77543307+EmineOzbek@users.noreply.github.com>
2022-03-12 08:42:50 +0300

    Fourth homework

---
+++ src/TaksimetreHesaplama.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TaksimetreHesaplama {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Km'yi girin: ");
+
+		double km = input.nextDouble();
+		double tutar = 10;
+
+		tutar += km * 2.20;
+
+		tutar = tutar < 20 ? 20 : tutar;
+		System.out.println("Toplam tutar: " + tutar);
+
+		input.close();
+	}
+}
